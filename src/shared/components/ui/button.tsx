795beb443f015b9e0089ae4f9a4eaa9392b47d09
khaled3li-gif <elmasrycloud@gmail.com>
2026-07-18 19:56:44 +0300

    feat(button-checkbox-components): implement button-checkbox-components

--- src/shared/components/ui/button.tsx
+++ src/shared/components/ui/button.tsx
@@ -2,29 +2,32 @@ import { Button as ButtonPrimitive } from '@base-ui/react/button';
 import { cva, type VariantProps } from 'class-variance-authority';
 
 import { cn } from '@/shared/lib/utils';
+import { LoaderCircle } from 'lucide-react';
 
 const buttonVariants = cva(
-  "group/button inline-flex shrink-0 items-center justify-center rounded-md border border-transparent bg-clip-padding text-sm font-medium whitespace-nowrap transition-all outline-none select-none focus-visible:border-ring focus-visible:ring-3 focus-visible:ring-ring/50 active:not-aria-[haspopup]:translate-y-px disabled:pointer-events-none disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-3 aria-invalid:ring-destructive/20 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+  "group/button inline-flex shrink-0 items-center justify-center rounded-lg cursor-pointer border border-transparent bg-clip-padding text-base font-medium whitespace-nowrap transition-all outline-none select-none focus-visible:border-ring focus-visible:ring-3 focus-visible:ring-ring/50 active:not-aria-[haspopup]:translate-y-px disabled:pointer-events-none disabled:bg-ds-bg-soft disabled:text-ds-text-muted aria-invalid:border-destructive aria-invalid:ring-3 aria-invalid:ring-destructive/20 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
   {
     variants: {
       variant: {
-        default: 'bg-primary text-primary-foreground hover:bg-primary/80',
+        default:
+          'border-ds-border-soft bg-ds-bg-muted text-ds-text-primary hover:bg-ds-bg-subtle text-[var(--text-plain)]',
+
+        primary: 'bg-ds-bg-primary text-ds-text-inverse hover:bg-ds-bg-primary-saturated',
+
         outline:
-          'border-border bg-background shadow-xs hover:bg-muted hover:text-foreground aria-expanded:bg-muted aria-expanded:text-foreground dark:border-input dark:bg-input/30 dark:hover:bg-input/50',
+          'border-ds-border-primary bg-ds-bg-plain text-ds-text-primary hover:bg-ds-bg-primary-fade disabled:border-transparent',
+
         secondary:
-          'bg-secondary text-secondary-foreground hover:bg-[color-mix(in_oklch,var(--secondary),var(--foreground)_5%)] aria-expanded:bg-secondary aria-expanded:text-secondary-foreground',
-        ghost:
-          'hover:bg-muted hover:text-foreground aria-expanded:bg-muted aria-expanded:text-foreground dark:hover:bg-muted/50',
-        destructive:
-          'bg-destructive/10 text-destructive hover:bg-destructive/20 focus-visible:border-destructive/40 focus-visible:ring-destructive/20 dark:bg-destructive/20 dark:hover:bg-destructive/30 dark:focus-visible:ring-destructive/40',
+          'bg-ds-bg-primary-fade text-ds-text-primary hover:bg-ds-bg-primary-faint text-[var(--text-plain)]',
+
+        ghost: 'bg-ds-bg-plain hover:bg-ds-bg-soft bg-ds-bg-subtle hover:bg-ds-bg-plain',
+
+        destructive: 'bg-ds-bg-danger hover:bg-ds-bg-danger-saturated text-ds-text-inverse',
+
         link: 'text-primary underline-offset-4 hover:underline',
       },
       size: {
-        default:
-          'h-9 gap-1.5 px-2.5 in-data-[slot=button-group]:rounded-md has-data-[icon=inline-end]:pr-2 has-data-[icon=inline-start]:pl-2',
-        xs: "h-6 gap-1 rounded-[min(var(--radius-md),8px)] px-2 text-xs in-data-[slot=button-group]:rounded-md has-data-[icon=inline-end]:pr-1.5 has-data-[icon=inline-start]:pl-1.5 [&_svg:not([class*='size-'])]:size-3",
-        sm: 'h-8 gap-1 rounded-[min(var(--radius-md),10px)] px-2.5 in-data-[slot=button-group]:rounded-md has-data-[icon=inline-end]:pr-1.5 has-data-[icon=inline-start]:pl-1.5',
-        lg: 'h-10 gap-1.5 px-2.5 has-data-[icon=inline-end]:pr-2 has-data-[icon=inline-start]:pl-2',
+        default: 'w-45 gap-1.5 px-4 py-3.5',
         icon: 'size-9',
         'icon-xs':
           "size-6 rounded-[min(var(--radius-md),8px)] in-data-[slot=button-group]:rounded-md [&_svg:not([class*='size-'])]:size-3",
@@ -40,18 +43,33 @@ const buttonVariants = cva(
   }
 );
 
+interface IButtonProps {
+  isLoading?: boolean;
+  loadingText?: string;
+}
+
 function Button({
   className,
+  isLoading = false,
+  loadingText = 'loading...',
+  children,
+  disabled,
   variant = 'default',
   size = 'default',
   ...props
-}: ButtonPrimitive.Props & VariantProps<typeof buttonVariants>) {
+}: ButtonPrimitive.Props & VariantProps<typeof buttonVariants> & IButtonProps) {
   return (
     <ButtonPrimitive
+      disabled={disabled || isLoading}
       data-slot="button"
       className={cn(buttonVariants({ variant, size, className }))}
       {...props}
-    />
+    >
+      <span className="inline-flex items-center gap-2">
+        {isLoading ? loadingText : children}
+        {isLoading && <LoaderCircle className="animate-spin" />}
+      </span>
+    </ButtonPrimitive>
   );
 }
 
